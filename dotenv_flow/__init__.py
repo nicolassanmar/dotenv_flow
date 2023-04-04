0b4c8a43511a6423cfc402bfd87e736754499b11
Nicolás San Martín <nicolassanmar@gmail.com>
2023-04-04 13:35:14 -0300

    Feat: Support .env files

--- dotenv_flow/__init__.py
+++ dotenv_flow/__init__.py
@@ -16,19 +16,22 @@ _DEFAULTS = [".env.defaults", ".env"]
 def dotenv_flow(
     env: Optional[str] = os.getenv("PY_ENV", ...),
     base_path: Optional[os.PathLike] = None,
+    load_global_env: Optional[bool] = False,
     **kwargs: Mapping[str, Any],
 ) -> Sequence[str]:
     """
     Load dotenv files based on the value of the PY_ENV variable, if set.
     Values in more specific files override previous values.
-    Files have 2 flavors:
+    Files have 3 flavors:
+        - global: (ex: .env) that should be committed to version control
         - public (ex: .env.dev) that should be committed to version control
         - private (ex: .env.dev.local) that has preference over the previous one, if present
 
     dotenv files are searched and loaded with python-dotenv
 
-    :param env: The name of the environment. It can be set to None explicitly to remove the warning and run with defaults only
+    :param env: The name of the environment. It can be set to None explicitly to remove the warning and run with defaults only.
     :param base_path: If provided, the base path to load the files from. Otherwise, the current working directory is used.
+    :param load_global_env: Whether the '.env' file is loaded or not.
     :return: a list with the paths of the files loaded
     """
 
@@ -39,6 +42,9 @@ def dotenv_flow(
         names.append(f".env.{env}")
     elif env is not None:
         warnings.warn("no env selected, using defaults only")
+
+    if load_global_env:
+        names.append(".env")
 
     loaded = {}
     for name in reversed(names):
